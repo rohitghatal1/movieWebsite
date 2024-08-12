aa767f1d52defefdaf46e7271b1fa6ab7fd816a5
rohitghatal1 <rohitghatal@gmail.com>
2024-08-12 18:20:34 +0545

    added js to play and close video dynamically

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-function playmovie1() {
-    document.getElementById("video1").style.display = "block";
-    document.getElementById('background').classList.add('background-blur');
+// for playing swiper movies ----------------------------------------------------------
+function playSwiperVideo1(){
+    document.getElementById("swipervideo1").style.display = "block";
+}
+function playSwiperVideo2(){
+    document.getElementById("swipervideo2").style.display = "block";
+}
+function playSwiperVideo3(){
+    document.getElementById("swipervideo3").style.display = "block";
+}
+function playSwiperVideo4(){
+    document.getElementById("swipervideo4").style.display = "block";
+}
+function playSwiperVideo5(){
+    document.getElementById("swipervideo5").style.display = "block";
 }
 
-function playSwiperVideo(){
-    document.getElementById("swipervideo").style.display = "block";
+// for playing movies -----------------------------------------------------------------
+function playmovie1() {
+    document.getElementById("video1").style.display = "block";
 }
+
 function playmovie2() {
     document.getElementById("video2").style.display = "block";
 }
@@ -19,6 +33,26 @@ function playmovie5() {
     document.getElementById("video5").style.display = "block";
 }
 
+
+// for closing swiper movie  -------------------------------------------------------------
+function closeswiperVideo1(){
+    document.getElementById("swipervideo1").style.display = "none";
+}
+function closeswiperVideo2(){
+    document.getElementById("swipervideo2").style.display = "none";
+}
+function closeswiperVideo3(){
+    document.getElementById("swipervideo3").style.display = "none";
+}
+function closeswiperVideo4(){
+    document.getElementById("swipervideo4").style.display = "none";
+}
+function closeswiperVideo5(){
+    document.getElementById("swipervideo5").style.display = "none";
+}
+
+
+// for closing movies -------------------------------------------------------------
 function closeMovie1() {
     document.getElementById("video1").style.display = "none";
 }
