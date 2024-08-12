1801889593e89ee116258d0a21c3a2c7a00305d3
rohitghatal1 <rohitghatal@gmail.com>
2024-08-12 16:31:49 +0545

    modified code

---
+++ script.js
@@ -0,0 +1,39 @@
+function playmovie1() {
+    document.getElementById("video1").style.display = "block";
+    document.getElementById('background').classList.add('background-blur');
+}
+
+function playSwiperVideo(){
+    document.getElementById("swipervideo").style.display = "block";
+}
+function playmovie2() {
+    document.getElementById("video2").style.display = "block";
+}
+function playmovie3() {
+    document.getElementById("video3").style.display = "block";
+}
+function playmovie4() {
+    document.getElementById("video4").style.display = "block";
+}
+function playmovie5() {
+    document.getElementById("video5").style.display = "block";
+}
+
+function closeMovie1() {
+    document.getElementById("video1").style.display = "none";
+}
+function closeMovie2() {
+    document.getElementById("video2").style.display = "none";
+}
+function closeMovie3() {
+    document.getElementById("video3").style.display = "none";
+}
+
+function closeMovie4() {
+    document.getElementById("video4").style.display = "none";
+}
+
+function closeMovie5() {
+    document.getElementById("video5").style.display = "none";
+
+}
